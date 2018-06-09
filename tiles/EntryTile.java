291f64b60439e709057278c8ab0bc2b01c7b2552
smax253 <smax253@gmail.com>
2018-06-09 18:33:30 -0400

    Changed entrytile constructor
    Changed constructor to have maximum students on creation

--- tiles/EntryTile.java
+++ tiles/EntryTile.java
@@ -2,7 +2,7 @@ package tiles;
 
 public class EntryTile extends Tile {
 
-  public EntryTile(int[] coords, int capacity) { super(coords, capacity, 0); }
+  public EntryTile(int[] coords, int capacity) { super(coords, capacity, capacity); }
 
   public EntryTile(int[] coords, int capacity, int currentStudents) { super(coords, capacity, currentStudents); }
 
