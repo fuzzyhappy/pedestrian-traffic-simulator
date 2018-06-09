098e397f36573d2fc43a2533d044fff368c6f86f
smax253 <smax253@gmail.com>
2018-06-09 17:29:26 -0400

    Changed Tile references to int arrays

--- Student.java
+++ Student.java
@@ -1,19 +1,19 @@
 import java.lang.RuntimeException;
 
 public class Student{
-  Tile destination;
-  Tile location;
+  int[] destination;
+  int[] location;
   int steps;
   int starttime;
   int endtime;
   boolean inClass;
-  public Student(Tile loc, Tile dest, int start){
+  public Student(int[] loc, int[] dest, int start){
       destination = dest;
       steps = 0;
       starttime = start;
       inClass = false;
   }
-  public void updateLocation(Tile loc){
+  public void updateLocation(int[] loc){
     location = loc;
   }
   public void destReach(int end){
@@ -30,7 +30,7 @@ public class Student{
     return steps*1.0/(endtime-starttime);
   }
 
-  public Tile getLocation(){
+  public int[] getLocation(){
     return location;
   }
 
