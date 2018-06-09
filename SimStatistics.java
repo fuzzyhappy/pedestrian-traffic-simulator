63198c3826b944ec4943ed152dede06db7203471
smax253 <smax253@gmail.com>
2018-06-09 18:01:05 -0400

    Added a class to process simulation statistics

---
+++ SimStatistics.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+public class SimStatistics{
+  Map map;
+  ArrayList<Student> students;
+  public SimStatistics(Map finishedMap){
+    map = finishedMap;
+    students = finishedMap.getRemovedStudents();
+  }
+  private double pctLate(){
+    Iterator itr = students.iterator();
+    int numLate = 0;
+    while(itr.hasNext()){
+      if (itr.next().isTardy()) numLate++;
+    }
+    return numLate*1.0/students.size();
+  }
+  private double avgEff(){
+    Iterator itr = students.iterator();
+    double totaleff = 0;
+    while(itr.hasNext()){
+      totaleff  += itr.next().eff();
+    }
+  }
+  public String printStats(){
+    String s = "";
+    s+="Percentage of students late: "+ pctLate()*100+"%\n";
+    s+="Average efficiency: "+avgEff()*100;
+  }
+}
